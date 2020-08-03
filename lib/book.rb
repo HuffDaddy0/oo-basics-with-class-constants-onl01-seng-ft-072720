a7bbc8b223e92417b46b965835e81da432836db4
Tyler Huffman <ty.huff24@gmail.com>
2020-08-03 19:20:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,11 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title)
+  GENRES = []
+
+  def initialize(title, genre)
     @title = title
+    GENRES << genre
   end
 
   def turn_page
